00438caae4d635e4d2c8156d87426a51d4484c61
wajiha <w5428481@gmail.com>
2024-05-17 16:37:26 +0500

    updated work

--- crud-app/src/components/Todo.jsx
+++ crud-app/src/components/Todo.jsx
@@ -1,103 +1,124 @@
 import React, { useState } from 'react'
 import img from '../image/Todo-img.png'
 import { v4 as uuidv4 } from 'uuid';
+
+
 const Todo = () => {
-
-const [todos, setTodos] = useState([]);
-const [obj, setObj] = useState({
-    id: uuidv4(),
-    item: ''
-  });
-
-  function handleChange(e) {
-    setObj({
-      ...obj,
-      [e.target.name]: e.target.value
+    const [todos, setTodos] = useState([]);
+    const [obj, setObj] = useState({
+        id: uuidv4(),
+        item: ''
     });
-  }
+    const [isEditing, setIsEditing] = useState(false);
+    const [editId, setEditId] = useState(null);
 
-  function handleAdd() {
-if (obj.item !== '') {
-    setTodos([
-      ...todos,
-      obj
-    ]);
-    setObj({
-      id: uuidv4(),
-      item: ''
-    });
-}
-else{
-}
-  }
-
-  const DeleteAllItems = () => {
-    setTodos([])
- }
-
-  function handleDel(id) {
-    const DeleteTodos = todos?.filter(todo => todo.id !== id);
-    setTodos(DeleteTodos);
-    setObj({
-        item:''
-    })
-  }
-
-  function handleEdit(item) {
-    setObj(
-      {
-        ...obj,
-        item: item
-      }
-    )
-  }
-
-  function handleSave(item, id) {
-    const updatedTodos = todos.map(todo => {
-      if (todo.id === id) {
-        return { ...todo,
-          item: obj.item 
-        };
-        }
-        return todo;
-      });
+    function handleChange(e) {
         setObj({
-          item: ''
+            ...obj,
+            [e.target.name]: e.target.value
         });
-      setTodos(updatedTodos);
-  }
+    }
+
+    function handleAdd() {
+        if (obj.item !== '') {
+            if (isEditing && editId !== null) {
+                const updatedTodos = todos.map(todo => {
+                    if (todo.id === editId) {
+                        return {
+                            ...todo,
+                            item: obj.item
+                        };
+                    }
+                    return todo;
+                });
+                setTodos(updatedTodos);
+                setIsEditing(false);
+                setEditId(null);
+            } else {
+                setTodos([
+                    ...todos,
+                    obj
+                ]);
+            }
+            setObj({
+                id: uuidv4(),
+                item: ''
+            });
+        } else {
+            alert('Enter Todos.')
+        }
+    }
+
+    const DeleteAllItems = () => {
+        setTodos([])
+    }
+
+    function handleEdit(id, item) {
+        if (!isEditing) {
+            setObj({
+                id: id,
+                item: item
+            });
+            setIsEditing(true);
+            setEditId(id);
+        } else {
+            const updatedTodos = todos.map(todo => {
+                if (todo.id === editId) {
+                    return {
+                        ...todo,
+                        item: obj.item
+                    };
+                }
+                return todo;
+            });
+            setObj({
+                item: ''
+            });
+            setTodos(updatedTodos);
+            setIsEditing(false);
+            setEditId(null);
+        }
+    }
+
+    function handleDel(id) {
+        const DeleteTodos = todos?.filter(todo => todo.id !== id);
+        setTodos(DeleteTodos);
+        setObj({
+            item: ''
+        });
+        setIsEditing(false);
+        setEditId(null);
+    }
 
     return (
         <>
-    <div className="h-100 mt-16 w-full flex justify-center font-sans">
-    <div className="main rounded shadow p-6 m-4 w-full lg:w-3/4 lg:max-w-lg md:w-3/4 md:max-w-lg sm:w-3/4 sm:max-w-auto">
-        <div className="mb-5">
-            <h1 className="text-center text-xl font-bold text-grey-darkest">To-do List</h1>
-            <div className="flex mt-4">
-                <input onChange={handleChange} value={obj.item} name='item' type='text' className="shadow appearance-none border rounded w-full py-2 px-3 mr-4" placeholder="Add Todo"></input>
-                <button onClick={handleAdd} className="text-md w-24 ml-1 flex-no-shrink border-2 rounded">Add</button>
-                <button onClick={DeleteAllItems} className="text-md w-24 ml-1 flex-no-shrink px-1 border-2 rounded">DeleteAll</button>
-            </div>
-        </div>
-        <div>
-            <div>
-                {todos?.map(todo => (
-                <ul className='flex my-3 mx-5 '>
-                    <li className="w-full text-xl text-grey-darkest" key={todo?.id}>{todo?.item}</li>
-                    <div className='w-full flex justify-end'>
-                        <button onClick={() => handleDel(todo?.id)} className="text-sm px-2 mr-1 border-2 rounded ">Delete</button>
-                        <button onClick={() => handleEdit(todo?.item,todo?.id)} className="text-sm mr-1 px-2 border-2 rounded">Edit</button>
-                        <button onClick={() => handleSave(todo?.item,todo?.id)} className="text-sm  px-2 border-2 rounded">Save</button>
+            <div className="h-100 mt-16 w-full flex justify-center font-sans">
+                <div className="rounded shadow-lg shadow-lime-800  p-6 m-4 w-full sm:w-3/4 sm:max-w-full md:w-3/4 md:max-w-lg lg:w-3/4 lg:max-w-lg">
+                    <div className="mb-5">
+                        <h1 className="text-center text-2xl font-bold text-grey-darkest">To-do List</h1>
+                        <div className="flex mt-4">
+                            <input onChange={handleChange} value={obj.item} name='item' type='text' className="shadow appearance-none border rounded w-full py-1 px-3 mr-4" placeholder="Add Todo"></input>
+                            <button onClick={handleAdd} className="text-md  w-16 ml-1 flex-no-shrink  border-0 px-1 rounded text-white bg-[#5e835a]">Add</button>
+                            <button onClick={DeleteAllItems} className="text-md w-24 ml-1 flex-no-shrink px-1 border-0 rounded text-white bg-[#5e835a]">DeleteAll</button>
+                        </div>
                     </div>
-                </ul>
-                ))}
-                <img src={img} alt="" />
+                    <div>
+                        <div className=''>
+                            {todos?.map(todo => (
+                                <ul className='w-full flex my-2 justify-between lg:px-5 md:px-5' key={todo?.id}>
+                                    <li className="w-full text-xl text-grey-darkest">{todo?.item}</li>
+                                    <div className='w-full flex justify-end'>
+                                        <button onClick={() => handleDel(todo?.id)} className="text-sm px-1 mr-1 border-0 rounded text-white bg-[#5e835a]">Delete</button>
+                                        <button onClick={() => handleEdit(todo?.id, todo?.item)} className="text-sm mr-1 px-1 border-0 rounded text-white bg-[#5e835a]">{isEditing && editId === todo.id ? 'Save' : 'Edit'}</button>
+                                    </div>
+                                </ul>
+                            ))}
+                            <img src={img} alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
         </>
     )
 }
-
 export default Todo
